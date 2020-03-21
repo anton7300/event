164b3972e4859f93c97c0e805519cd2e318d9d89
vashchuk <anton7300@gmail.com>
2020-03-21 23:12:56 +0300

    add models and controllers

---
+++ app/Http/Controllers/InterestCatController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\InterestCat;
+use Illuminate\Http\Request;
+
+class InterestCatController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\InterestCat  $interestCat
+     * @return \Illuminate\Http\Response
+     */
+    public function show(InterestCat $interestCat)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\InterestCat  $interestCat
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(InterestCat $interestCat)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\InterestCat  $interestCat
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, InterestCat $interestCat)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\InterestCat  $interestCat
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(InterestCat $interestCat)
+    {
+        //
+    }
+}
